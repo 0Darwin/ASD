62b543c45fd27ff2e0a3590ee2ee2e3235197776
Mohammed Benaissa <160794219+0Darwin@users.noreply.github.com>
2024-10-19 17:16:09 +0200

    Update exo1-1.c

--- exo1-1.c
+++ exo1-1.c
@@ -2,24 +2,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-bool prime(int n){
-    bool is_prime;
-    is_prime=true;
-    for (int i=2 ; i<=sqrt(n) ; i++){
-        if(n%i==0){
-            is_prime=false;
-            return is_prime;
+
+bool prime(int n) {
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
         }
     }
-
+    return true;
 }
-int main(){
+
+int main() {
     int a;
-    printf("enter a number between 1 and 10^9:");
-    scanf("%d",&a);
-    if(prime(a)==false)
-        printf("the number is not prime");
-    else
-        printf("is prime ");
+    printf("enter number between 1 and 10^9: ");
+    scanf("%d", &a);
+    
+    if (prime(a)) {
+        printf("number is prime\n");
+    } else {
+        printf(" number is not prime\n");
+    }
+    
     return 0;
 }
